2b84b1ca87b1ae67d4ccb45f7df0907dfc3870a1
Carrollyn Catanzaro <endyrekkyrts@yahoo.com>
2018-02-20 18:18:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,4 +19,11 @@ function deepestChild() {
 
 function increaseRankBy(n) {
   const rankedLists = document.querySelectorAll('.ranked-list')
+  
+  for (let i = 0, length = ranked-list.length; i < length; i++) {
+    let children = rankedLists[i].children
+    
+    for (let j = 0, chLength = children.length; j < chLength; j++)
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+  }
 } 

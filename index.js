735df199bd4394ef8c09747e272c22b8734c84dd
Carrollyn Catanzaro <endyrekkyrts@yahoo.com>
2018-02-20 18:15:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ function getFirstSelector(s) {
 }
 
 function nestedTarget() {
-  
+  return document.querySelector(`#nested .target`)
 }
 
 function deepestChild() {
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
   
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+  return node
 }  
 
 function increaseRankBy(n) {
-  
+  const rankedLists = document.querySelectorAll('.ranked-list')
 } 
